9255a3e85df72593577372a60c66dfc22ca6eab2
viveralia <edgarv10@gmail.com>
2019-10-06 15:53:02 -0500

    Signup/login conditional rendering + favicon

--- frontend/src/components/Layout/Navbar.jsx
+++ frontend/src/components/Layout/Navbar.jsx
@@ -1,8 +1,9 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { Link } from 'react-router-dom'
 import ButtonLink from '../Buttons/ButtonLink'
 import { name } from '../../constants/meta.json'
 import styled from 'styled-components'
+import { FiZap } from 'react-icons/fi'
 
 /*************************/
 /********* CSS ***********/
@@ -14,8 +15,14 @@ const StyledNav = styled.nav`
   padding: 1rem 1.5rem;
   a:first-child {
     font-weight: 500;
+    text-transform: lowercase;
     color: var(--black);
     font-size: 1.25rem;
+    display: flex;
+    align-items: center;
+    span {
+      margin-right: 0.25rem;
+    }
   }
 `
 
@@ -23,10 +30,17 @@ const StyledNav = styled.nav`
 /********* JSX ***********/
 /*************************/
 const Navbar = () => {
+  const [activeUser] = useState(JSON.parse(localStorage.getItem('activeUser')))
+
   return (
     <StyledNav>
-      <Link to='/'>{name}</Link>
-      <ButtonLink to='/login'>Log in</ButtonLink>
+      <Link to='/'>
+        <span>
+          <FiZap />
+        </span>
+        {name}
+      </Link>
+      {activeUser ? <ButtonLink to='/logout'>Log out</ButtonLink> : <ButtonLink to='/login'>Log in</ButtonLink>}
     </StyledNav>
   )
 }
